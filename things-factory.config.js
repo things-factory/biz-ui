fd18b93044d7b8550fbdbd55d54431d01c56c952
leemyunggyu <jaylee@hatiolab.com>
2019-05-21 20:32:53 +0900

    Initial commit

---
+++ things-factory.config.js
@@ -0,0 +1,13 @@
+import route from './client/route'
+import bootstrap from './client/bootstrap'
+
+export default {
+  route,
+  routes: [
+    {
+      tagname: 'biz-ui-main',
+      page: 'biz-ui-main'
+    }
+  ],
+  bootstrap
+}
